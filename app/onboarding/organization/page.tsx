181364abd5e9fd43f0e8bfd16a7a39073d1d71bc
AccelAnalysis <jholman@accelanalysis.com>
2026-08-18 18:20:52 -0400

    Make organization onboarding routes addressable by workflow step

--- app/onboarding/organization/page.tsx
+++ app/onboarding/organization/page.tsx
@@ -1,5 +1,17 @@
 import OrganizationSelectionClient from "./OrganizationSelectionClient";
+import {
+  organizationContextFromSearchParams,
+  organizationStepFromSearchParams,
+} from "@/lib/onboarding/organization";
 
-export default function OrganizationSelectionPage() {
-  return <OrganizationSelectionClient />;
+export default async function OrganizationSelectionPage({
+  searchParams,
+}: {
+  searchParams: Promise<Record<string, string | string[] | undefined>>;
+}) {
+  const params = await searchParams;
+  const context = organizationContextFromSearchParams(params);
+  const initialStep = organizationStepFromSearchParams(params, context);
+
+  return <OrganizationSelectionClient initialContext={context} initialStep={initialStep} />;
 }
