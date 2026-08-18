c7ee258442ca711a2c943112e81578594a489da2
AccelAnalysis <jholman@accelanalysis.com>
2026-08-18 18:19:08 -0400

    Route industry and service leaves to canonical editors

--- components/onboarding/capability-enrichment-route.tsx
+++ components/onboarding/capability-enrichment-route.tsx
@@ -2,9 +2,17 @@
 
 import { useSearchParams } from "next/navigation";
 import CapabilityEnrichment from "./capability-enrichment";
+import CapabilityProfileContextEditor from "./capability-profile-context-editor";
 
 export default function CapabilityEnrichmentRoute({ path }: { path: string[] }) {
   const searchParams = useSearchParams();
   const organizationId = searchParams.get("organizationId") ?? undefined;
+
+  if (path[0] === "industry-services" && path[1] === "industries-served") {
+    return <CapabilityProfileContextEditor organizationId={organizationId} field="industries" />;
+  }
+  if (path[0] === "industry-services" && path[1] === "service-offerings") {
+    return <CapabilityProfileContextEditor organizationId={organizationId} field="service_offerings" />;
+  }
   return <CapabilityEnrichment path={path} organizationId={organizationId} />;
 }
