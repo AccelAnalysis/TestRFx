84977b4a773b4a96488cdab0e53b224f8623c8eb
AccelAnalysis <jholman@accelanalysis.com>
2026-08-22 12:45:41 -0400

    Render semantic record action icon IDs through shared adapter

--- components/exchange/record-actions.tsx
+++ components/exchange/record-actions.tsx
@@ -2,12 +2,11 @@
 
 import type { LensAction } from "@/lib/exchange/contracts";
 import { getLensActionUnavailableReason, isLensActionEnabled } from "@/lib/exchange/action-registry";
-import { ExchangeIcon } from "./exchange-nav-icon";
+import { ExchangeIcon, isExchangeUiIconId } from "./exchange-nav-icon";
 import styles from "./record-actions.module.css";
 
 function actionIcon(action: LensAction) {
-  if (action.id === "match-rfx") return <ExchangeIcon icon="match-rfx" size={14} />;
-  return action.icon;
+  return isExchangeUiIconId(action.icon) ? <ExchangeIcon icon={action.icon} size={14} /> : action.icon;
 }
 
 export function RecordActionRow({
